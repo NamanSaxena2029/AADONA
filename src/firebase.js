e7407458a85bdf24426a7601c24bd50b03e33a43
NamanSaxena2029 <ramagrawal99001@gmail.com>
2026-02-23 21:35:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3Hb8qmjK4VjvO2gHS6ZYhPZgYTRLhTvo",
-  authDomain: "aadona-cms.firebaseapp.com",
-  projectId: "aadona-cms",
-  storageBucket: "aadona-cms.firebasestorage.app",
-  messagingSenderId: "998691072043",
-  appId: "1:998691072043:web:2b09f1d3329462dd76626e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
