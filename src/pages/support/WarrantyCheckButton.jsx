021a2da3ecd541700ff288ff7f001a5cacf89370
NainDembani1 <dembaninain16@gmail.com>
2026-03-25 15:21:44 +0530

    Update WarrantyCheckButton.jsx

--- src/pages/support/WarrantyCheckButton.jsx
+++ src/pages/support/WarrantyCheckButton.jsx
@@ -124,10 +124,10 @@ const WarrantyCheckButton = () => {
        >
          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
            <div className="text-center">
-             <h1 className="text-4xl font-bold text-white sm:text-5xl md:text-6xl">
+             <h1 className="text-5xl font-bold text-white sm:text-5xl md:text-6xl">
                Warranty
              </h1>
-             <p className="mt-6 text-xl text-white max-w-3xl mx-auto">
+             <p className="mt-6 text-md text-white max-w-3xl mx-auto">
                Please provide the serial number and invoice to check your warranty status.
              </p>
            </div>
