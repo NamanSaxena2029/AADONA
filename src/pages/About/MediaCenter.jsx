8b477b40477d68b57bbc4d22de9aa46d6ff55a8d
NainDembani1 <dembaninain16@gmail.com>
2026-03-31 11:55:44 +0530

    Update MediaCenter.jsx

--- src/pages/About/MediaCenter.jsx
+++ src/pages/About/MediaCenter.jsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
 import bg from '../../assets/bg.jpg';
-import mediacenterbanner from '../../assets/MediaCentwhite.jpeg';
+import mediacenterbanner from '../../assets/MediaCenterBanner.jpeg';
 
 /* -------- Structured Data (JSON-LD) for SEO -------- */
 const structuredData = {
