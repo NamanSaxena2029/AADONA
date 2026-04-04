ea2783541c924a68d685b8263c0da4c649d16b4d
NainDembani1 <dembaninain16@gmail.com>
2026-04-04 09:34:09 +0530

    Update MissionVission.jsx

--- src/pages/About/MissionVission.jsx
+++ src/pages/About/MissionVission.jsx
@@ -122,8 +122,7 @@ const MissionVision = () => {
                           aria-label="Mission herbanner"
                         >
                           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-                            <h1 className="text-5xl font-bold text-
-                            white sm:text-5xl md:text-6xl">
+                            <h1 className="text-5xl font-bold text-white sm:text-5xl md:text-6xl">
                              Mission &amp; Vision
                             </h1>
                             <p className="mt-6 text-md text-white max-w-3xl mx-auto">
